cc2321361fded10c3a613757c825b49d296bc82e
wjh <wangjh@xiniaogongkao.com>
2020-09-01 10:03:43 +0800

    提交样式重置文件

--- src/main.js
+++ src/main.js
@@ -1,21 +1,22 @@
-import 'amfe-flexible/index.js';
-import Vue from 'vue';
-import App from './App.vue';
-import store from './store';
-import router from './router';
-import VConsole from 'vconsole';
-import './styles/index.scss';
-Vue.config.productionTip = false;
+import 'amfe-flexible/index.js'
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+import router from './router'
+import VConsole from 'vconsole'
+import './styles/index.less'
+import './styles/border.less'
+Vue.config.productionTip = false
 
-import { isNeedConsole } from '../config';
-import 'vant/lib/index.css';; // Vant 样式
+import { isNeedConsole } from '../config'
+import 'vant/lib/index.css' // Vant 样式
 // 开启调试器
 if (isNeedConsole) {
-    var vConsole = new VConsole();
+  var vConsole = new VConsole()
 }
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app');
+  render: (h) => h(App),
+}).$mount('#app')
